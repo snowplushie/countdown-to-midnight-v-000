a259ff1dd076e761a59219107ee7d091bfeda683
Paige Wiener <snowplushie@yahoo.com>
2019-02-28 21:34:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(time)
   while time > 0
-  puts "#{time} SECONDS!"
+  puts "#{time} SECOND(S)!"
   time -= 1
 end
 "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(time)
   while time > 0
-  puts "#{time} SECONDS!"
+  puts "#{time} SECOND(S)!"
   sleep(1)
   time -= 1
 end
